e8dfcd794968024aa8b82782b0214d205f62e1df
Gabriel Silipi <gabriel.silipi@gmail.com>
2021-01-05 16:01:26 -0300

    idade dinamica e working dev

--- components/About/About.js
+++ components/About/About.js
@@ -2,27 +2,30 @@ import React from 'react'
 import styles from './About.module.css'
 
 export default function About() {
+  const birthDate = new Date(1999, 8, 13);
+  const now = new Date();
+
   return (
     <div className={styles.container}>
 
       <h1 className={styles.title}>Sobre mim...</h1>
-      <h2>Olá, é um prazer em ter você como visitante do meu site! </h2>
+      <h2>Olá, é um prazer em ter você como visitante do meu site!</h2>
       <br />
       <p>Então quer dizer que você quis saber mais sobre a minha pessoa?! Fico feliz por isto.</p><br />
-      <p>Então... meu nome é Gabriel Silipi, eu tenho 21 anos, atualmente estou trabalhando como <b>Analista de Quality Assurance</b> na empresa <b>Wealth Systems</b> e está sendo meu primeiro emprego em um time de desenvolvimento ágil, onde estou fazendo o que realmente gosto e, está sendo um grande aprendizado para mim e para a minha carreira. Além disso, faço alguns projetos paralelos, por exemplo, este site, que é feito para mostrar os outros projetos paralelos que estou fazendo também. <i>Projetoception</i>.<br/>
-      Todos estes projetos que faço são para aprender mais sobre novas ferramentas e sobre o mundo de desenvolvimento Web, pois, além de me aperfeiçoar em testes, também tenho como objetivo um dia me tornar um <b>desenvolvedor front-end</b>.</p><br />
+      <p>Então... meu nome é Gabriel Silipi, eu tenho {~~((now - birthDate) / 31557600000)} anos, atualmente estou trabalhando como <b>Desenvolvedor de Sistemas</b> na empresa <b>LINX</b> e está sendo minha primeira experiência como desenvolvedor profissional, e está sendo muito gratificante. Além disso, faço alguns projetos paralelos, por exemplo, este site, que é feito para mostrar os outros projetos paralelos que estou fazendo também. <i>Projetoception</i>.<br/>
+      Todos estes projetos que faço são para aprender mais sobre novas ferramentas e sobre o mundo de desenvolvimento Web.</p><br />
       <p>Criei este portfólio/blog (blogfólio, ou portblog, como quiser chamar) com a intenção de ter um local seguro para armazenar meus projetos e poder mostrar à outras pessoas. A maioria dos projetos daqui são de uso pessoal, mas se você se interessar por algum deles ou quiser saber mais sobre como foi construido, não hesite em me procurar, podemos conversar sobre, ou quem sabe, trabalhar juntos em cima dele!</p>
       <p>Estou totalmente aberto à sugestões, criticas construtivas sempre são bem vindas.</p><br/>
       <h3>Sobre a construção deste site:</h3><br/>
       <p>Foi desenvolvido utilizando <b>React</b>, juntamente com o framework <b>Next.js</b> (que deixou este site BEM rápido e responsivo) e sem nenhum framework CSS.<br/>
-      Hospedado no Netlify.</p>
+      Hospedado na Vercel.</p>
 
       <div className={styles.socialMedia}>
 
-        <div onClick={() => {window.open(process.env.URL, "_blank")}} className={styles.socialMediaLink}>
+        <div onClick={() => {window.open("https://www.facebook.com/gabriel-silipi-1", "_blank")}} className={styles.socialMediaLink}>
           <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook" class="svg-inline--fa fa-facebook fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"></path></svg>
         </div>
-        <div onClick={() => {window.open("https://www.instagram.com/sil.ipi/", "_blank")}} className={styles.socialMediaLink}>
+        <div onClick={() => {window.open("https://www.instagram.com/sil.ipii/", "_blank")}} className={styles.socialMediaLink}>
           <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" class="svg-inline--fa fa-instagram fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg>
         </div>
         <div onClick={() => {window.open("https://twitter.com/silipg", "_blank")}} className={styles.socialMediaLink}>
